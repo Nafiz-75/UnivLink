f2eb96e46e0f1b85e54561cd18213b48efd5533e
Md <rahatilrahaman0@gmail.com>
2025-04-16 03:41:33 +0600

    class create

--- lib/screens/drawer_navigation.dart
+++ lib/screens/drawer_navigation.dart
@@ -2,3 +2,19 @@ import 'package:flutter/material.dart';
 import 'home_page.dart';
 import 'my_events_page.dart';
 import 'event_management_page.dart';
+
+class DrawerNavigation extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: ListView(
+        padding: EdgeInsets.zero,
+        children: [
+          DrawerHeader(
+            decoration: BoxDecoration(color: Colors.blue),
+            child: Text(
+              'UnivLink',
+              style: TextStyle(color: Colors.white, fontSize: 24),
+            ),
+          ),
+
