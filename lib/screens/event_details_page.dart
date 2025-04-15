3b1d1ca6e173e20a018475affa623261162e8052
Md <rahatilrahaman0@gmail.com>
2025-04-16 03:48:22 +0600

    Event details

--- lib/screens/event_details_page.dart
+++ lib/screens/event_details_page.dart
@@ -2,3 +2,17 @@ import 'package:flutter/material.dart';
 import '../models/event_model.dart';
 import '../services/event_service.dart';
 import 'package:intl/intl.dart';
+
+class EventDetailsPage extends StatefulWidget {
+  final String eventId;
+  final String userId;
+
+  EventDetailsPage({required this.eventId, required this.userId});
+
+  @override
+  _EventDetailsPageState createState() => _EventDetailsPageState();
+}
+
+class _EventDetailsPageState extends State<EventDetailsPage> {
+  final EventService _eventService = EventService();
+  late Future<EventModel> _event;
