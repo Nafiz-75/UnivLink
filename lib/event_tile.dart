82b526319b3c0e3ee57b3818ebe11b6eb3a27a34
Nafiz-75 <imtiaz15-5075@diu.edu.bd>
2025-04-16 04:10:15 +0600

    OK

--- lib/event_tile.dart
+++ lib/event_tile.dart
@@ -15,3 +15,30 @@ class EventTile extends StatelessWidget {
     required this.onTap,
     this.trailing,
   }) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Card(
+        margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
+        elevation: 4,
+        child: ListTile(
+          contentPadding: const EdgeInsets.symmetric(
+            horizontal: 16,
+            vertical: 12,
+          ),
+          title: Text(
+            title,
+            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+          ),
+          subtitle: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [SizedBox(height: 4), Text(date), Text(location)],
+          ),
+          trailing: trailing,
+        ),
+      ),
+    );
+  }
+}
