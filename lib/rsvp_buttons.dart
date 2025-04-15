4f1c8f72e3c981ef65344621e6f571b103e5e11f
Nafiz-75 <imtiaz15-5075@diu.edu.bd>
2025-04-16 03:57:20 +0600

    commit

--- lib/rsvp_buttons.dart
+++ lib/rsvp_buttons.dart
@@ -13,3 +13,26 @@ class RSVPButtons extends StatelessWidget {
     required this.isGoing,
     required this.isInterested,
   });
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        IconButton(
+          onPressed: onGoing,
+          icon: Icon(
+            Icons.check_circle,
+            color: isGoing ? Colors.green : Colors.grey,
+          ),
+        ),
+        IconButton(
+          onPressed: onInterested,
+          icon: Icon(
+            Icons.star,
+            color: isInterested ? Colors.amber : Colors.grey,
+          ),
+        ),
+      ],
+    );
+  }
+}
